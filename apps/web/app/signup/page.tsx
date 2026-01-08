f67fe5814c020b4ecb35fe86b699051843694237
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2026-01-08 11:09:03 +0530

    made signup page client component

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,4 +1,6 @@
 // apps/web/app/signup/page.tsx
+"use client";
+
 import dynamic from "next/dynamic";
 const AuthForm = dynamic(() => import("@/features/auth/components/AuthForm"), { ssr: false });
 
